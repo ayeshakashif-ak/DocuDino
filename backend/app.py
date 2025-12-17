0679aaef761f934eda7f608c29fba37e665a7eab
Ayesha Kashif <126335752+ayeshakashif-ak@users.noreply.github.com>
2025-12-17 17:53:15 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -4,4 +4,4 @@ import os
 if __name__ == "__main__":
     debug_mode = os.environ.get("FLASK_ENV", "development") == "development"
     # Binding to 0.0.0.0 allows connections from any IP
-    app.run(host="0.0.0.0", port=5002, debug=false)
+    app.run(host="0.0.0.0", port=5002, debug=False)
